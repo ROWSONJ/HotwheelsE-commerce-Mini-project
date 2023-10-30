aeb8c8dd660ed0c1b27e265eb2a622e74f4c1439
Ajcharee Pakapikul <erne.ajcharee02@gmail.com>
2023-10-31 03:49:06 +0700

    profile

--- views/profile.php
+++ views/profile.php
@@ -153,16 +153,14 @@
       
       .profile{
         position: fixed;
-        z-index:3;
-        translate: -50% -50%;
         width:25%;
         height: 45%;
-        padding: 70px 30px 44px 30px;
+        padding: 70px 30px 44px ;
         border-radius: 1rem;
         background: #fff;
         text-align: center;
-        margin-top: 180px;
-        margin-left: 200px;
+        margin-top: 20px;
+        margin-left: 10px;
 
       }
       .profile img {
@@ -199,16 +197,14 @@
       
       .buyer{
         position: fixed;
-        z-index:3;
-        translate: -50% -50%;
         width:25%;
         height: 45%;
-        padding: 70px 30px 44px 30px;
+        padding: 70px 30px 44px ;
         border-radius: 1rem;
         background: #fff;
         text-align: center;
-        margin-top: 180px;
-        margin-left: 200px;
+        margin-top: 20px;
+        margin-left: 10px;
 
       }
       .buyer h2{
@@ -225,17 +221,15 @@
       }
 
       .seller{
-        position: fixed;
-        z-index:3;
-        translate: -50% -50%;
+        position: fixed;  
         width:25%;
         height: 45%;
-        padding: 70px 30px 44px 30px;
+        padding: 70px 30px 44px ;
         border-radius: 1rem;
         background: #fff;
         text-align: center;
-        margin-top: 180px;
-        margin-left: 200px;
+        margin-top: 20px;
+        margin-left: 10px;
 
       }
       .seller h2{
@@ -303,7 +297,7 @@
       <hr>
       <li>
         <!-- Button trigger modal -->
-        <button type="button" class="nav-link link-body-emphasis" data-bs-toggle="modal" data-bs-target="#exampleModal">
+        <button type="button" class="nav-link link-body-emphasis" data-bs-toggle="modal" data-bs-target="#deleteModal">
           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
             <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z"/>
           </svg>
@@ -311,7 +305,7 @@
         </button>
 
 <!-- Modal -->
-        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal fade" id="deleteModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
           <form action="deleteacc.php" method="POST" id="deleteForm">
           <div class="modal-dialog modal-dialog-centered">
               <div class="modal-content">
@@ -441,7 +435,7 @@
 
           <div class="position-absolute top-0 end-0">
             <!-- Button trigger modal -->
-              <button type="button" class="btn btn-outline-secondary" style="margin:20px;" data-bs-toggle="modal" data-bs-target="#exampleModal">
+              <button type="button" class="btn btn-outline-secondary" style="margin:20px;" data-bs-toggle="modal" data-bs-target="#editModal">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16">
                   <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"></path>
                 </svg>
@@ -487,7 +481,7 @@
 
 <!-- Modal -->
 
-<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<div class="modal fade" id="editModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog">
     <div class="modal-content">
       <form action="editprofile.php" method="post" enctype="multipart/form-data"> 
